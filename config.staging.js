418ff2f5ed7db5fae183f4e7802d2dced08487a1
André Nogueira <andre.nogueira@epfl.ch>
2021-03-13 09:32:47 +0100

    chore: add graasp staging config
    that will be picked up when NODE_ENV = 'staging'

---
+++ config.staging.js
@@ -0,0 +1,7 @@
+(function () {
+    module.exports = {
+        SKIP_IFRAMELY_RENDERS: true,
+        port: 8061, // can be overridden by PORT env var
+        RESPONSE_TIMEOUT: 5 * 1000 // ms
+    };
+})();
