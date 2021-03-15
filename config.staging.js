32697472e033886fb149ce007ce22fb58da9be01
André Nogueira <andre.nogueira@epfl.ch>
2021-03-15 11:24:30 +0100

    fix: iframely timeout in docker+newer node
    by disabling HTTP2

--- config.staging.js
+++ config.staging.js
@@ -2,6 +2,7 @@
     module.exports = {
         SKIP_IFRAMELY_RENDERS: true,
         port: 8061, // can be overridden by PORT env var
-        RESPONSE_TIMEOUT: 5 * 1000 // ms
+        RESPONSE_TIMEOUT: 5 * 1000, // ms
+        DISABLE_HTTP2: true // https://github.com/itteco/iframely/issues/275
     };
 })();
